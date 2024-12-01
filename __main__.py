0e3590b3712a4986bc842e6cfbb5465e637e72cb
Skynet-dj <dibyajyotid722@gmail.com>
2024-12-01 23:20:23 +0530

    idk

--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
 from utils.requirements import install_requirements
-from cli import main as cli_main
+
 
 def main():
     install_requirements()
-    cli_main()
+    import cli
+    cli.main()
 
 if __name__ == "__main__":
     main()
