0756eb5bc8ab95e87a4071d030b51ba719d5d091
Skynet-dj <dibyajyotid722@gmail.com>
2024-12-10 19:47:25 +0530

    idk

--- scripts/commands/rm.py
+++ scripts/commands/rm.py
@@ -1,6 +1,8 @@
 import os
+from scripts.com_util import update_project
 
-def rm_project(path: str) -> None:
+def rm_project(path, name) -> None:
+    update_project(path, name,True)
     os.remove(path)
     return True
 
