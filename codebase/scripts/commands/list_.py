70746bd89d84812b00485ee156b87b44e1b50166
Skynet-dj <dibyajyotid722@gmail.com>
2024-12-27 21:37:00 +0530

    idk

--- codebase/scripts/commands/list_.py
+++ codebase/scripts/commands/list_.py
@@ -30,6 +30,7 @@ def list_projects(flags=None):
             all_paths = [all_paths[all_names.index(name)] for name in all_names]
         if flags:
             console.print("[bold green]Not a valid flag[/bold green]")
+            return
     else:
         for project_name in projects.keys():
             all_names.append(project_name)
